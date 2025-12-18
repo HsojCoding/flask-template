6cb20b941ac7ef1167214cd8083b9e2606186fd3
Josh <joshhadderz@gmail.com>
2025-12-18 08:36:27 +0000

    Blueprint setups with basic login functionality

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,3 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, url_for, render_template
+from flask_login import login_user
+from app.db.models import User
+from werkzeug.security import check_password_hash
 
 auth_bp = Blueprint('auth_bp', __name__)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('main_bp.index'))
+        else:
+            return redirect(url_for('auth_bp.login', error='Invalid username or password'))
+    return render_template('auth/login.html')
--- app/views/main.py
+++ app/views/main.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 main_bp = Blueprint('main_bp', __name__)
+
+@main_bp.route('/')
+@login_required
+def index():
+    return render_template('main/index.html')
