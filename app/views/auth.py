f3440cbb96f915fe92f3731f70fa8854ff07cd77
Josh <joshhadderz@gmail.com>
2025-12-19 14:56:24 +0000

    Added login failed flash message

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, url_for, render_template
+from flask import Blueprint, request, redirect, url_for, render_template, flash
 from flask_login import login_user
 from app.db.models import User
 from werkzeug.security import check_password_hash
@@ -15,5 +15,6 @@ def login():
             login_user(user)
             return redirect(url_for('main_bp.index'))
         else:
-            return redirect(url_for('auth_bp.login', error='Invalid username or password'))
+            flash('Invalid username or password', 'error')
+            return redirect(url_for('auth_bp.login'))
     return render_template('auth/login.html')
